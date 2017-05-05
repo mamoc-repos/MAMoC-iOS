6a73cb4a502ca51954203fbd34251917c63f6abb
dawand <Su>
2017-05-05 01:09:00 +0100

    Initial Commit

---
+++ MobileCloud/MobileCloud.h
@@ -0,0 +1,19 @@
+//
+//  MobileCloud.h
+//  MobileCloud
+//
+//  Created by Dawand Sulaiman on 05/05/2017.
+//  Copyright © 2017 StAndrews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MobileCloud.
+FOUNDATION_EXPORT double MobileCloudVersionNumber;
+
+//! Project version string for MobileCloud.
+FOUNDATION_EXPORT const unsigned char MobileCloudVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MobileCloud/PublicHeader.h>
+
+
